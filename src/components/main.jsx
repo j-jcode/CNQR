717b8a95c0fbd1aa539fdddbd0bb0d3799f614ec
jmason32 <jmason32@ufl.edu>
2023-02-22 15:43:51 -0500

    Video fix

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -4,7 +4,7 @@ import videoBG from '../CNQR.mp4';
 const main = () => {
   return (
     <div className='main'>
-        <video src={videoBG} autoPlay loop mute/>
+        <video src={videoBG} autoPlay={true} loop={true} muted={true} playsInline={true}/>
     </div>
   )
 }
